e4a61c0648e940f6ca64450dafb467241780b392
Sofi.F <fleytassofiaaylen@gmail.com>
2026-06-13 01:50:30 -0300

    extraction and profiling of the dataset

--- src/extract/download_data.py
+++ src/extract/download_data.py
@@ -1,9 +1,47 @@
-from pathlib import Path
+import requests
 
-RAW_DATA_PATH = Path("data/raw")
+from src.utils.config import (
+    DATASET_URL,
+    DATASET_FILENAME,
+    RAW_DIR
+)
 
-def main():
-    print("Data extraction process initialized")
+def download_dataset():
+    print("Downloading dataset...")
+
+    response = requests.get(           # Hacer una petición HTTP GET
+        DATASET_URL,
+        timeout=30                     # Esperar máximo 30 segundos
+    )
+
+    response.raise_for_status()         # Si el servidor devuelve: 404 500 403 Python lanza una excepción automáticamente.
+
+    return response
+
+
+def save_dataset(response):
+    RAW_DIR.mkdir(               # Crea la carpeta si no existe.
+        parents=True,
+        exist_ok=True
+    )
+
+    file_path = RAW_DIR / DATASET_FILENAME    # data/raw/higher_education.csv
+
+    with open(file_path, "wb") as file:     #Los archivos descargados deben guardarse en binario.
+        file.write(response.content)
+
+    print(f"Saved to: {file_path}")
+
+
+def main():                               # Flujo ETL
+    print("Starting extraction process")
+
+    response = download_dataset()
+
+    save_dataset(response)
+
+    print("Extraction completed")
+
 
 if __name__ == "__main__":
     main()
--- src/utils/config.py
+++ src/utils/config.py
@@ -6,4 +6,6 @@ RAW_DIR = BASE_DIR / "data" / "raw"
 
 PROCESSED_DIR = BASE_DIR / "data" / "processed"
 
-DATASET_URL = "PENDIENTE"
+DATASET_URL = ("https://www.argentina.gob.ar/sites/default/files/00._educ_superior_datos.csv")
+
+DATASET_FILENAME = "higher_education.csv"
